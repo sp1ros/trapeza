778c01e0c26d60f80b7a668c6ba5b87b5161f83f
sp1ros <59471161+sp1ros@users.noreply.github.com>
2020-01-07 22:20:18 +0200

    Add files via upload

--- projectaki1.py
+++ projectaki1.py
@@ -1,4 +1,3 @@
-from random import randint
 class pelatis:
     def __init__(self):
         self.onoma=""
@@ -28,39 +27,36 @@ class logariasmos:
         self.poso=0
 
     def addpelati(self,p):
-        print (self.code)
         self.pel.append(p)
-        p.show()
+        
 
         
     def katathesi(self,katath):
-        self.poso= self.poso + katathesi
+        self.poso= self.poso + katath
+        print("Νέο υπόλοιπο= ",self.poso)
 
     def analipsi(self,analip):
         if self.poso>analip:
             self.poso=self.poso - analip
+            print("Νέο υπόλοιπο= ",self.poso)
         else:
             print("ποσο αναληψης μεγαλυτερο απο υπολοιπο")
 
     
     def show(self):
         
-        print("Kodikos:",self.code)
-        print("Ypoloipo:",self.poso)
-        for p in self.pel:
-            p.show()
+        print("Υπόλοιπο: ",self.poso)
+        
+
+
+    
 
 def menu():
     P=[]
     L=[]
 
 
-    p1=pelatis()
-    p2=pelatis()
-    p1.setpel('p1','231243','11')
-    p2.setpel('p2','245532','22')
-    P.append(p1)
-    P.append(p2)
+  
     while(True):
         try:
             print("1. Εισαγωγή νέου πελάτη")
@@ -106,13 +102,107 @@ def menu():
                         i=i-1
                 l.show()
                 L.append(l)
-            elif(x==10):
+            elif(x==3):
+                a=int(input("Αριθμός Λογαριασμού :"))
+                fnd=0
+                for katlog in L:
+                    if(katlog.code==a):
+                        kata=int(input("Ποσό κατάθεσης :"))
+                        katlog.katathesi(kata)
+                        fnd=1
+                        
+                if(fnd==0):
+                    print("Δεν υπάρχει ο λογαριασμός")
+
+            elif(x==4):
+                a=int(input("Αριθμός Λογαριασμού :"))
+                fnd=0
+                for katlog in L:
+                    if(katlog.code==a):
+                        an=int(input("Ποσό ανάληψης :"))
+                        katlog.analipsi(an)
+                        fnd=1
+                        
+                if(fnd==0):
+                    print("Δεν υπάρχει ο λογαριασμός")
+            elif(x==5):
+                
+                ip=int(input("αριθμός λογαριαάσμου :"))
+                fnd=0
+                for katlog in L:
+                    if(katlog.code==ip):
+                        katlog.show()
+                        fnd=1
+                if(fnd==0):
+                    print("Δεν υπάρχει ο λογαριασμός")
+            elif(x==6):
+                af=input("δώσε ΑΦΜ πελάτη :")
+                s=0
+                for i in L:
+                    for j in i.pel:
+                       if (j.afm==af):
+                           s+=i.poso
+                print("συνολικο υπλοιπο πελατη =",s)
+
+                
+            elif(x==7):
+                s=0
+                for j in L:
+                    s+=j.poso
+                print("συνολικό τράπεζας = ",s)
+                    
+                
+            elif(x==8):
+                file=open("data.txt","w")
+                file.write(str(len(P))+"\n")
+                file.write(str(len(L))+"\n")
+            
+                for pp in P:
+                    file.write(pp.onoma + "\n")
+                    file.write(pp.tilefono + "\n")
+                    file.write(pp.afm + "\n")
+                for ll in L:
+                    file.write(str(ll.code)+"\n")
+                    file.write(str(ll.poso)+"\n")
+                    file.write(str(len(ll.pel))+"\n")
+                    for p2 in ll.pel:
+                        file.write(p2.afm+"\n")
+                file.close()
+                    
+            elif(x==9):
+                file=open("data.txt","r")
+                
+                n1=int(file.readline())
+                n2=int (file.readline())
+                for i in range(0,n1):
+                    o=file.readline().replace("\n","")
+                    t=file.readline().replace("\n","")
+                    a=file.readline().replace("\n","")
+                    p=pelatis()
+                    p.setpel(o,t,a)
+                    P.append(p)
+                for i in range(0,n2):
+                    l=logariasmos()
+                    c=file.readline()
+                    l.code=int(c)
+                    ps=file.readline()
+                    l.poso=int(ps)
+                    n3=int(file.readline())
+                    for j in range(0,n3):
+                        a=file.readline().replace("\n","")
+                        p1=pelatis()
+                        p1.setpel("","",a)
+                        l.addpelati(p1)    
+                    L.append(l)
+                file.close()
+                
+            else:
                 for pp in P:
                     pp.show()
                     print("-------------------------")
             
-        except:
-           print ("error2")
+        except Exception as e:
+           print ("error2 ",e)
 
 
 menu()  
